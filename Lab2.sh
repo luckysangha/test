fe7872bb285fdb7f20d0a982087a44a277875f55
luckysangha <60719572+luckysangha@users.noreply.github.com>
2024-09-21 17:04:35 -0400

    Update Lab2.sh

--- Lab2.sh
+++ Lab2.sh
@@ -11,15 +11,14 @@ bq query --use_legacy_sql=false \
 OPTIONS(model_type="logistic_reg") AS
 SELECT
   IF(totals.transactions IS NULL, 0, 1) AS label,
-  IFNULL(device.operatingSystem, "") AS os,
+  COALESCE(device.operatingSystem, "") AS os,
   device.isMobile AS is_mobile,
-  IFNULL(geoNetwork.country, "") AS country,
-  IFNULL(totals.pageviews, 0) AS pageviews
+  COALESCE(geoNetwork.country, "") AS country,
+  COALESCE(totals.pageviews, 0) AS pageviews
 FROM
-  `bigquery-public-data.google_analytics_sample.ga_sessions_*`
-WHERE
-  _TABLE_SUFFIX BETWEEN "20160801" AND "20170631"
+  `bigquery-public-data.google_analytics_sample.ga_sessions_20170101`
 LIMIT 100000;'
+
 
 # Step 3: Evaluate the model
 echo "Evaluating the model..."
