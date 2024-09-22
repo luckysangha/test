968b27ce6f45125306f30979b41645fb0b7cbfce
luckysangha <60719572+luckysangha@users.noreply.github.com>
2024-09-22 12:34:25 -0400

    Update Lab4.sh

--- Lab4.sh
+++ Lab4.sh
@@ -6,41 +6,46 @@ bq query --nouse_legacy_sql '
 CREATE OR REPLACE MODEL `ecommerce.classification_model`
 OPTIONS
 (
-  model_type="logistic_reg",
-  labels = ["will_buy_on_return_visit"]
+  model_type = 'logistic_reg',
+  input_label_cols = ['will_buy_on_return_visit'],
+  max_iterations = 10  -- Aggressively reduce iterations for faster training
 )
 AS
-#standardSQL
+
+WITH features AS (
+  SELECT
+    fullVisitorId,
+    IFNULL(totals.bounces, 0) AS bounces,
+    IFNULL(totals.timeOnSite, 0) AS time_on_site
+  FROM
+    `data-to-insights.ecommerce.web_analytics`
+  WHERE
+    totals.newVisits = 1
+    AND date BETWEEN '20160801' AND '20170430'  -- Training period
+),
+
+return_visitors AS (
+  SELECT
+    fullVisitorId,
+    COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0 AS will_buy_on_return_visit
+  FROM
+    `data-to-insights.ecommerce.web_analytics`
+  WHERE
+    date BETWEEN '20160801' AND '20170430'
+  GROUP BY fullVisitorId
+)
+
 SELECT
-  * EXCEPT(fullVisitorId)
+  f.bounces,
+  f.time_on_site,
+  IF(rv.will_buy_on_return_visit, 1, 0) AS will_buy_on_return_visit
 FROM
-  -- features
-  (
-    SELECT
-      fullVisitorId,
-      IFNULL(totals.bounces, 0) AS bounces,
-      IFNULL(totals.timeOnSite, 0) AS time_on_site
-    FROM
-      `data-to-insights.ecommerce.web_analytics`
-    WHERE
-      totals.newVisits = 1
-      AND date BETWEEN "20160801" AND "20170430"  -- train on first 9 months
-  )
-  JOIN
-  (
-    SELECT
-      fullVisitorId,
-      IF(
-        COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0,
-        1,
-        0
-      ) AS will_buy_on_return_visit
-    FROM
-      `data-to-insights.ecommerce.web_analytics`
-    GROUP BY
-      fullVisitorId
-  )
-  USING (fullVisitorId);'
+  features f
+LEFT JOIN
+  return_visitors rv
+USING (fullVisitorId)
+WHERE RAND() < 0.25;  -- Use a more aggressive sampling rate of 25%
+
   
 bq query --nouse_legacy_sql '
 SELECT
